ac22a270fb8ebbdcf1fcac74d67ba6dcb2a672b1
fardin-hasan <19-40043-1@student.aiub.edu>
2021-12-19 14:47:06 +0600

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Home</title>
+</head>
+<body>
+@extends('layouts.app')
+@section('content')
+<div class="d-flex justify-content-center align-items-center" style="height: 90vh;">
+<div>
+        <h1>Welcome to homepage</h1>
+    </div>
+@endsection
+</body>
+</html>
